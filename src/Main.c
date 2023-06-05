22b3cfdcfd998b947de1136c83f574b0a2fff82e
Steffen Beyer <steffen.beyer@213tec.com>
2023-06-05 15:22:48 +0200

    DMA and IRQ based demos working

--- src/Main.c
+++ src/Main.c
@@ -1,108 +1,336 @@
 /********************************** (C) COPYRIGHT *******************************
  * File Name          : Main.c
- * Author             : WCH
- * Version            : V1.0
- * Date               : 2020/08/06
- * Description        : 串口1收发演示
- *********************************************************************************
- * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
- * Attention: This software (modified or not) and binary are used for 
- * microcontroller manufactured by Nanjing Qinheng Microelectronics.
- *******************************************************************************/
+ * Author             : Steffen Beyer
+ * Version            : 0.0.1
+ * Date               : 2023/06/05
+ * Description        : Timer capture mode interrupt demo
+ *********************************************************************************/
 
+#include <math.h>
 #include "CH58x_common.h"
 
-uint8_t TxBuff[] = "This is a tx exam\r\n";
-uint8_t RxBuff[100];
-uint8_t trigB;
+#define CAPTURE_SIZE 50
+
+float targetFreq;
+
+BOOL CapDMA;
+volatile uint16_t CapPointer;
+volatile BOOL CapTimeout;
+volatile BOOL CapDMA_End;
+
+// DMA buffer
+__attribute__((aligned(4))) uint32_t CapBuf[CAPTURE_SIZE];
+
+/*********************************************************************
+ * @fn      CyclesPerUs
+ *
+ * @brief   Calculate number of clock cycles per μs
+ *
+ * @return  number of cycles
+ */
+uint32_t CyclesPerUs(uint32_t us)
+{
+    return (float)FREQ_SYS / 1e6 * us;
+}
+
+/*********************************************************************
+ * @fn      ClockInit
+ *
+ * @brief   Setup clock output
+ *
+ * @return  none
+ */
+void ClockInit(void)
+{
+    GPIOA_ModeCfg(GPIO_Pin_12, GPIO_ModeOut_PP_5mA);
+    GPIOA_ModeCfg(GPIO_Pin_13, GPIO_ModeOut_PP_5mA);
+
+    targetFreq = (float)FREQ_SYS / (uint8_t)(FREQ_SYS / 256000) / 256;
+    PWMX_CLKCfg(FREQ_SYS / 256000); // f = ~256 kHz (error 0,16%)
+    PWMX_CycleCfg(PWMX_Cycle_256); // f2 = 1 kHz, T = 1 ms
+    PWMX_ACTOUT(CH_PWM4, 256 / 2, Low_Level, ENABLE);
+    PWMX_ACTOUT(CH_PWM5, 256 / 2, Low_Level, ENABLE);
+}
+
+/*********************************************************************
+ * @fn      DMA_Init
+ *
+ * @brief   Setup timer for DMA capture
+ *
+ * @return  none
+ */
+void DMA_Init(void)
+{
+    // PB10 instead of PA10
+    GPIOPinRemap(ENABLE, RB_PIN_TMR1);
+    GPIOB_ModeCfg(GPIO_Pin_10, GPIO_ModeIN_PU);
+
+    TMR1_CapInit(Edge_To_Edge);
+    TMR1_DMACfg(ENABLE, (uint16_t)(uint32_t)&CapBuf[0], (uint16_t)(uint32_t)&CapBuf[CAPTURE_SIZE], Mode_Single);
+    TMR1_CAPTimeoutCfg(50 * (FREQ_SYS / 1000)); // 50 ms timeout
+    TMR1_ITCfg(ENABLE, TMR1_2_IT_DMA_END | TMR0_3_IT_CYC_END); // Turn on DMA completion interrupt
+}
+
+/*********************************************************************
+ * @fn      DMA_Run
+ *
+ * @brief   Start capture
+ *
+ * @return  none
+ */
+void DMA_Run(void)
+{
+    printf("\nrunning DMA capture...\n");
+    CapDMA = TRUE;
+
+    CapTimeout = FALSE;
+    CapDMA_End = FALSE;
+    // also resets the timer, so we get a valid first measurement
+    TMR1_CapInit(Edge_To_Edge);
+    TMR1_DMACfg(ENABLE, (uint16_t)(uint32_t)&CapBuf[0], (uint16_t)(uint32_t)&CapBuf[CAPTURE_SIZE], Mode_Single);
+    PFIC_EnableIRQ(TMR1_IRQn);
+}
+
+/*********************************************************************
+ * @fn      DMA_Stop
+ *
+ * @brief   Stop capture
+ *
+ * @return  none
+ */
+void DMA_Stop(void)
+{
+    PFIC_DisableIRQ(TMR1_IRQn);
+    R8_TMR1_CTRL_DMA = 0;
+}
+
+/*********************************************************************
+ * @fn      IRQ_Init
+ *
+ * @brief   Setup timer for IRQ capture
+ *
+ * @return  none
+ */
+void IRQ_Init(void)
+{
+    // PB11 instead of PA11
+    GPIOPinRemap(ENABLE, RB_PIN_TMR2);
+    GPIOB_ModeCfg(GPIO_Pin_11, GPIO_ModeIN_PU);
+
+    TMR2_CapInit(Edge_To_Edge);
+    TMR2_CAPTimeoutCfg(50 * (FREQ_SYS / 1000)); // 50 ms timeout
+    TMR2_ITCfg(ENABLE, TMR0_3_IT_DATA_ACT | TMR0_3_IT_CYC_END); // Turn on data activity interrupt
+}
+
+/*********************************************************************
+ * @fn      IRQ_Run
+ *
+ * @brief   Start capture
+ *
+ * @return  none
+ */
+void IRQ_Run(void)
+{
+    printf("\nrunning IRQ capture...\n");
+    CapDMA = FALSE;
+
+    CapPointer = 0;
+    CapTimeout = FALSE;
+    // also resets the timer, so we get a valid first measurement
+    TMR2_CapInit(Edge_To_Edge);
+    PFIC_EnableIRQ(TMR2_IRQn);
+}
+
+/*********************************************************************
+ * @fn      IRQ_Stop
+ *
+ * @brief   Stop capture
+ *
+ * @return  none
+ */
+void IRQ_Stop(void)
+{
+    PFIC_DisableIRQ(TMR2_IRQn);
+}
+
+/*********************************************************************
+ * @fn      Pause
+ *
+ * @brief   Short break for the user
+ *
+ * @return  none
+ */
+void Pause(void)
+{
+    printf("pausing for 5 seconds...\n");
+    DelayMs(5000);
+}
+
+/*********************************************************************
+ * @fn      DisplayResults
+ *
+ * @brief   Analyse the captured data and output
+ *
+ * @return  none
+ */
+void DisplayResults(void)
+{
+    uint32_t avgSum = 0;
+    uint16_t avgCount = 0;
+    uint16_t avgSkip = 0;
+
+    // how many edges did we capture?
+    uint16_t capSize = CapDMA ? (R16_TMR1_DMA_NOW - R16_TMR1_DMA_BEG) / sizeof(int32_t) : CapPointer;
+    if (CapDMA && CapTimeout && capSize > 0) {
+        // drop last value if DMA timeout
+        capSize -= 1;
+    }
+
+    if (capSize > 0) {
+        // display 5 usec values per line
+        printf("captured edge intervals:\n");
+        for (uint16_t i = 0; i < (capSize - 1) / 5 + 1; i++) {
+            uint16_t start = i * 5;
+
+            for (uint8_t j = 0; j < 5 && start + j < capSize; j++) {
+                int32_t edge = CapBuf[start + j] & (1 << 25);
+                int32_t cycles = CapBuf[start + j] & (1 << 25) - 1;
+                float usec = cycles * 1e6 / FREQ_SYS;
+                printf(edge ? "/" : "\\");
+                printf("%4ld,%02d   ", (uint32_t)usec, (uint8_t)(usec * 100) % 100);
+
+                // drop first sample for average calculatoin
+                if (avgSkip > 0) {
+                    avgSum += cycles;
+                    avgCount += 1;
+                } else {
+                    avgSkip += 1;
+                }
+            }
+            printf(" [usec]\n");
+        }
+
+        if (avgCount > 0) {
+            float freq = 0.5 / ((float)avgSum / avgCount / FREQ_SYS);
+            float error = fabsf(targetFreq - freq) / targetFreq * 100;
+            printf("frequency (avg.): %ld,%02d Hz (error = %ld,%02d %%)\n",
+                (uint32_t)freq, (uint8_t)(freq * 100) % 100,
+                (uint32_t)error, (uint8_t)(error * 100) % 100);
+        }
+    } else {
+        printf("no edges captured...\n");
+    }
+
+    if (CapTimeout) {
+        printf("stopped due to timeout.\n");
+    }
+}
+
+/*********************************************************************
+ * @fn      DebugInit
+ *
+ * @brief   Setup UART as console
+ *
+ * @return  none
+ */
+void DebugInit(void)
+{
+    GPIOA_SetBits(GPIO_Pin_9);
+    GPIOA_ModeCfg(GPIO_Pin_8, GPIO_ModeIN_PU);
+    GPIOA_ModeCfg(GPIO_Pin_9, GPIO_ModeOut_PP_5mA);
+    UART1_DefInit();
+}
 
 /*********************************************************************
  * @fn      main
  *
- * @brief   主函数
+ * @brief   Entry point
  *
  * @return  none
  */
 int main()
 {
-    uint8_t len;
-
     SetSysClock(CLK_SOURCE_PLL_60MHz);
+    // Setup UART1
+    // PA8: RXD, PA9: TXD
+    DebugInit();
 
-    /* Configure serial port 1: configure IO port mode first, then configure serial port */
-    GPIOA_SetBits(GPIO_Pin_9);
-    GPIOA_ModeCfg(GPIO_Pin_8, GPIO_ModeIN_PU);      // RXD-Configuration pull-up input
-    GPIOA_ModeCfg(GPIO_Pin_9, GPIO_ModeOut_PP_5mA); // TXD-Configure push-pull output, note that the IO port output high first
-    UART1_DefInit();
+    ClockInit();
+    DMA_Init();
+    IRQ_Init();
 
-#if 1 // Test serial port sending string
-    UART1_SendString(TxBuff, sizeof(TxBuff));
+    printf("Start @ChipID=%02X\n", R8_CHIP_ID);
+    printf("(PWM4) PA12: 1 kHz* clock output (push pull)\n");
+    printf("(PWM5) PA13: 1 kHz* clock output (push pull)\n");
+    printf("(TMR1) PB10: input DMA (pull up)\n");
+    printf("(TMR2) PB11: input IRQ (pull up)\n");
+    printf("* %ld,%02d Hz\n", (uint32_t)targetFreq, (uint8_t)(targetFreq * 100) % 100);
 
-#endif
-
-#if 1 // Query method: Receive data and send it out
-    while(1)
-    {
-        len = UART1_RecvString(RxBuff);
-        if(len)
-        {
-            UART1_SendString(RxBuff, len);
+    while(1) {
+        IRQ_Run();
+        // wait for capture to finish (timeout 5s)
+        for (uint16_t i = 0; i < 50 && CapPointer < CAPTURE_SIZE && !CapTimeout; i++) {
+            DelayMs(100);
         }
+        IRQ_Stop();
+        DisplayResults();
+        Pause();
+
+        DMA_Run();
+        // wait for capture to finish (timeout 5s)
+        for (uint16_t i = 0; i < 50 && !CapDMA_End && !CapTimeout; i++) {
+            DelayMs(100);
+        }
+        DMA_Stop();
+        DisplayResults();
+        Pause();
     }
-
-#endif
-
-#if 0 // Interrupt mode: receive data and send it out
-    UART1_ByteTrigCfg(UART_7BYTE_TRIG);
-    trigB = 7;
-    UART1_INTCfg(ENABLE, RB_IER_RECV_RDY | RB_IER_LINE_STAT);
-    PFIC_EnableIRQ(UART1_IRQn);
-#endif
-
-    while(1);
 }
 
 /*********************************************************************
- * @fn      UART1_IRQHandler
+ * @fn      TMR1_IRQHandler
  *
- * @brief   UART1中断函数
+ * @brief   TMR1 demo handler (DMA)
  *
  * @return  none
  */
 __INTERRUPT
 __HIGH_CODE
-void UART1_IRQHandler(void)
+void TMR1_IRQHandler(void)
 {
-    volatile uint8_t i;
+    if (TMR1_GetITFlag(TMR1_2_IT_DMA_END)) {
+        TMR1_ClearITFlag(TMR1_2_IT_DMA_END);
+        DMA_Stop();
+        CapDMA_End = TRUE;
+    } else if (TMR1_GetITFlag(TMR0_3_IT_CYC_END)) {
+        TMR1_ClearITFlag(TMR0_3_IT_CYC_END);
+        DMA_Stop();
+        CapTimeout = TRUE;
+    }
+}
 
-    switch(UART1_GetITFlag())
-    {
-        case UART_II_LINE_STAT: // 线路状态错误
-        {
-            UART1_GetLinSTA();
-            break;
+/*********************************************************************
+ * @fn      TMR2_IRQHandler
+ *
+ * @brief   TMR2 demo handler (IRQ)
+ *
+ * @return  none
+ */
+__INTERRUPT
+__HIGH_CODE
+void TMR2_IRQHandler(void)
+{
+    if (TMR2_GetITFlag(TMR0_3_IT_DATA_ACT)) {
+        TMR2_ClearITFlag(TMR0_3_IT_DATA_ACT);
+        if (CapPointer < CAPTURE_SIZE) {
+            CapBuf[CapPointer] = R32_TMR2_FIFO;
+            CapPointer += 1;
+        } else {
+            IRQ_Stop();
         }
-
-        case UART_II_RECV_RDY: // 数据达到设置触发点
-            for(i = 0; i != trigB; i++)
-            {
-                RxBuff[i] = UART1_RecvByte();
-                UART1_SendByte(RxBuff[i]);
-            }
-            break;
-
-        case UART_II_RECV_TOUT: // 接收超时，暂时一帧数据接收完成
-            i = UART1_RecvString(RxBuff);
-            UART1_SendString(RxBuff, i);
-            break;
-
-        case UART_II_THR_EMPTY: // 发送缓存区空，可继续发送
-            break;
-
-        case UART_II_MODEM_CHG: // 只支持串口0
-            break;
-
-        default:
-            break;
+    } else if (TMR2_GetITFlag(TMR0_3_IT_CYC_END)) {
+        TMR2_ClearITFlag(TMR0_3_IT_CYC_END);
+        IRQ_Stop();
+        CapTimeout = TRUE;
     }
 }
